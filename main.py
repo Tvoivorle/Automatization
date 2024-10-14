ec073e9437b0b99fda1d22a6af978e0188f026d3
Stepan <stepan57skm@gmail.com>
2024-10-14 15:58:46 +0300

    Добавил свое имя Вики

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Hello!!')
 
-print('Степан Сергеев')
+print ('Степан Сергеев')
+print ('Виктория Солопенко')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
