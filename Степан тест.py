bebaafc7b1629a41e8366e894dcf84492828c699
Stepan <stepan57skm@gmail.com>
2024-10-15 14:39:10 +0300

    Добавил фильтрацию по статусам. Немного изменена проверка информации об отфильтрованных данных.

--- Степан тест.py
+++ Степан тест.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 def openFile(path_file=None):
     excel_file = 'C://МДТ//АСОЗ ЦТ 2025 1 этап.xlsx'
     df = pd.read_excel(excel_file)
@@ -6,26 +7,17 @@ def openFile(path_file=None):
 
 data, columns = openFile()
 
-# Вывод количества значений в ИЗНАЧАЛЬНОЙ таблице
-num_values = data.size  # Общее количество значений
-num_rows, num_cols = data.shape  # Количество строк и столбцов
-print(f"Общее количество значений в таблице: {num_values}")
-print(f"Количество строк: {num_rows}, Количество столбцов: {num_cols}")
-print(columns)  # Для вывода названий столбцов
-
-#Фильтрация по "Московская ЖД"
+# Фильтрация по Московской ЖД
 def filterDepartment(data, columns):
-    # Заголовок для фильтрации
     department_column = 'ЖД'
     if department_column in columns:
-        # Фильтруем данные
+        # Фильтруем данные по Московской ЖД
         filter_data = data[data[department_column] == 'Московская ЖД']
         return filter_data
     else:
         print(f"Ошибка: Столбец '{department_column}' не найден. Доступные столбцы: {list(columns)}")
         return None
 
-# Фильтруем данные
 filtered_data = filterDepartment(data, columns)
 
 # Фильтрация по статусу заявки
@@ -41,7 +33,6 @@ def filterStatus(filtered_data, columns):
         print(f"Ошибка: Столбец '{status_column}' не найден. Доступные столбцы: {list(columns)}")
         return None
 
-# Проверяем и выводим информацию об отфильтрованных данных
 if filtered_data is not None:
     status_filtered_data = filterStatus(filtered_data, columns)
 
